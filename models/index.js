f802a47684bc541ce0a59548ac75bd065bf0005b
mdpais <mdpais@gmail.com>
2023-02-10 20:11:57 -0400

    category routes created, not working

--- models/index.js
+++ models/index.js
@@ -20,7 +20,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  //as: 'product_tags'
+  as: 'product_tags'
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -29,7 +29,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
-  //as: 'tag_product'
+  as: 'tag_product'
 });
 
 module.exports = {
