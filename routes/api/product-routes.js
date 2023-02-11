01bbbdf2171185ac637163481ed8a54fa7c66e7d
mdpais <mdpais@gmail.com>
2023-02-11 16:43:48 -0400

    all done

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -68,9 +68,9 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update product data
-  Product.update(req.body, {
+  const products = await Product.update(req.body, {
     where: {
       id: req.params.id,
     },
@@ -109,8 +109,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Product deleted!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
